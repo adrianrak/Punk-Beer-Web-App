2a20dd6ed04ac6ab7b54a794b8d5d40a34a86169
Adrian Rak <adrrak@tlen.pl>
2018-07-31 21:21:06 +0200

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
 import Beer from './components/beer';
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Navbar />
         <Beer />
       </div>
     );
