1e6ac6c6907ae704083971fb66e56b84a572efe0
Adrian Rak <adrrak@tlen.pl>
2018-08-01 23:18:14 +0200

    try to paginate

--- src/components/beer.jsx
+++ src/components/beer.jsx
@@ -6,13 +6,14 @@ export default class Beer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            beers: []
+            beers: [],
+            currentPage: 0
 
         }
     }
 
     componentDidMount() {
-        fetch('https://api.punkapi.com/v2/beers?page=2&per_page=80')
+        fetch('https://api.punkapi.com/v2/beers?page=1&per_page=80')
         .then(response => response.json())
         .then(data => {
             console.log(data);
@@ -20,19 +21,33 @@ export default class Beer extends Component {
             this.setState({beers: beers})
         })
     }
+
+    previousPage = () => {
+        if (this.state.currentPage !== 1)
+          this.setState((prevState) => ({currentPage: (prevState.currentPage - 1)}))
+    }
+      
+    nextPage = () => {
+        if (this.state.currentPage + 1 < this.state.beers.lenght)
+          this.setState((prevState) => ({currentPage: (prevState.currentPage + 1)}))
+    }
+
     render() {
         return (
         <div>    
             <div className="container">
                 <h1>Hello!</h1>
                 <div className="row">
-                    {this.state.beers.map((data) =>
+                    {this.state.beers.slice((this.state.currentPage * 20), 20).map((data) =>
                     <div key={data.id} className="col-md-4 border border-primary">
                         <h5>{data.name}</h5>
                        <img src={data.image_url}></img>
                     </div>
                     )}
+                    
                 </div>
+                <button onClick={this.previousPage}>Previous Page</button>
+                <button onClick={this.nextPage}>Next Page</button>
             </div>
         </div>  
     );
